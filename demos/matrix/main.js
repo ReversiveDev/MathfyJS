75152b4c9f2ea7ce7f79dbed1d3e19af2c70ce5b
ReversiveDev <caetanorichard41@gmail.com>
2022-02-28 20:02:55 -0300

    Multiplicação de matrizes

--- demos/matrix/main.js
+++ demos/matrix/main.js
@@ -1,12 +1,31 @@
 import { Matrix } from "../../lib/Mathfy.js";
 
 
-let m1 = new Matrix();
+var m1 = new Matrix();
 m1.set(0, 0, 3);
 m1.set(1, 1, 5);
-let m2 = new Matrix();
-m2.set(0, 0, 4);
+m1.set(2, 2, 2);
+var m2 = new Matrix(1, 3);
+m2.set(0, 0, 1);
+m2.set(0, 1, 2);
+m2.set(0, 2, 3);
+
+console.log(m1.toString());
+console.log("\t\tx");
+console.log(m2.toString());
+console.log("\t\t=");
+console.log(m1.mul(m2).toString());
+
+console.log("##################")
+
+var m1 = new Matrix();
+m1.set(0, 0, 3);
+m1.set(1, 1, 5);
+m1.set(2, 2, 2);
+var m2 = new Matrix();
+m2.set(0, 0, 1);
 m2.set(1, 1, 2);
+m2.set(2, 2, 3);
 
 console.log(m1.toString());
 console.log("\t\t-");
